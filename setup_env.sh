83ccc6408665ff45724b08e5b347ca343c0ae3bd
Riverxia <riverdalecoder@outlook.com>
2026-06-19 17:40:09 -0400

    (acm) Currently Listening To: Discord Memes - MAHIRR Memes

--- setup_env.sh
+++ setup_env.sh
@@ -10,7 +10,7 @@ echo "============================================================"
 
 # 1. Create a virtual environment using Python 3.11 (safe version for all wheels)
 echo "[1/5] Creating Python 3.11 virtual environment..."
-uv venv --no-project --python 3.11 .venv
+uv venv --clear --python 3.11 .venv
 source .venv/bin/activate
 
 # 2. Temporarily comment out the malformed rusty-manga-image-translator package
@@ -19,15 +19,15 @@ sed -i 's/^rusty-manga-image-translator/# &/' requirements.txt
 
 # 3. Use uv to install the 149 standard packages at lightning speed
 echo "[3/5] Installing dependencies with uv..."
-uv pip install --no-project -r requirements.txt
+uv pip install -r requirements.txt
 
 # 4. Use standard pip to install the malformed rusty-manga-image-translator package
 echo "[4/5] Installing rusty-manga-image-translator with standard pip..."
-.venv/bin/pip install --extra-index-url https://frederik-uni.github.io/manga-image-translator-rust/python/wheels/simple/ rusty-manga-image-translator
+pip install --python /marimo/.venv/bin/python --extra-index-url https://frederik-uni.github.io/manga-image-translator-rust/python/wheels/simple/ rusty-manga-image-translator
 
 # 5. Install gradio
 echo "[5/5] Installing gradio..."
-uv pip install --no-project gradio
+uv pip install gradio
 
 # 6. Restore requirements.txt
 sed -i 's/^# \(rusty-manga-image-translator\)/\1/' requirements.txt
